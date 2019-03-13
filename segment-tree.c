b175cfc85dd29b575d0f340fd75352416397bc8c
karthikhegde05 <karthikhegde05@gmail.com>
2019-03-13 12:41:06 +0530

    first

---
+++ segment-tree.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int* Build_ST(int* A, int n);
+
+int* Build_ST(int* A, int n){
+	int i;
+	for(i=0; i<n; i++)
+		ST[i+n-1] = i;
+	for(i=n-2; i>-1; i--)
+		ST[]
+}
+
+
